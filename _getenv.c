6aa7fe180ec3427a2dec8394d5b74675cba7f271
Zanokuhlemaziya <Zanokuhlemaziya.com>
2023-09-12 03:42:47 -0700

    update

---
+++ _getenv.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * getenv - Searches for enviroment variables
+ * @name: Name of env variable to search for
+ *
+ * Return: String.
+ */
+
+extern char **environ;
+
+char *_getenv(const char *name)
+{
+
+	char **env = environ;
+
+	while (*env != NULL)
+	{
+		char *current_env_var = *env;
+		int i = 0;
+
+		while (current_env_var[i] && current_env_var[i] != '=')
+		{
+			i++;
+		}
+
+		if (current_env_var[i] == '=')
+		{
+
+			if (strncmp(current_env_var, name, i) == 0)
+			{
+
+				return current_env_var + i + 1;
+
+			}
+		}
+
+		env++;
+	}
+
+	return NULL;
+
+}
---
+++ change_dir.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * getline - Gets input from stream
+ * @lineptr: pointer to stdin
+ * @size_t: pointer to memory
+ * @stream: stream
+ *
+ * Return: Signed Integer.
+ */
+
+ssize_t _getline(char **lineptr, size_t *n, FILE *stream)
+{
+	size_t pos = 0;
+	int c;
+	char *new_ptr;
+
+	if (*lineptr == NULL || *n == 0)
+	{
+		*n = 128;
+		*lineptr = (char *)malloc(*n);
+		if (*lineptr == NULL)
+		{
+			return -1;
+		}
+	}
+
+	while ((c = fgetc(stream)) != EOF)
+	{
+		if (pos >= *n - 1)
+		{
+
+			*n *= 2;
+			new_ptr = (char *)realloc(*lineptr, *n);
+			if (new_ptr == NULL)
+			{
+				return -1;
+			}
+
+			*lineptr = new_ptr;
+		}
+
+		(*lineptr)[pos++] = c;
+
+		if (c == '\n')
+		{
+			break;
+		}
+	}
+
+	(*lineptr)[pos] = '\0';
+	return pos;
+}
